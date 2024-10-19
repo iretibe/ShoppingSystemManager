8c9be96195d140cb7dbd5464c8639788975f5b66
Somad Yessoufou <yessouf2009@yahoo.fr>
2024-10-19 13:02:03 +0000

    Added distributed caching and redis to Basket service API

---
+++ ShoppingSystemManager/Services/Basket/ShoppingSystem.Basket.API/Cache/CachedBasketRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using ShoppingSystem.Basket.API.Models;
+using ShoppingSystem.Basket.API.Repositories;
+using System.Text.Json;
+
+namespace ShoppingSystem.Basket.API.Cache
+{
+    public class CachedBasketRepository(IBasketRepository repository, IDistributedCache distributedCache) : IBasketRepository
+    {
+        public async Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken)
+        {
+            await repository.DeleteBasketAsync(userName, cancellationToken);
+
+            await distributedCache.RemoveAsync(userName, cancellationToken);
+
+            return true;
+        }
+
+        public async Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken)
+        {
+            var cachedBasket = await distributedCache.GetStringAsync(userName, cancellationToken);
+
+            if(!string.IsNullOrEmpty(cachedBasket)) 
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket);
+
+            var basket = await repository.GetBasketAsync(userName, cancellationToken);
+            
+            await distributedCache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart basket, CancellationToken cancellationToken)
+        {
+            await repository.StoreBasketAsync(basket, cancellationToken);
+
+            await distributedCache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket),cancellationToken);
+
+            return basket;
+        }
+    }
+}
--- ShoppingSystemManager/Services/Basket/ShoppingSystem.Basket.API/Program.cs
+++ ShoppingSystemManager/Services/Basket/ShoppingSystem.Basket.API/Program.cs
@@ -1,5 +1,9 @@
 using Carter;
+using HealthChecks.UI.Client;
 using Marten;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
+using ShoppingSystem.Basket.API.Cache;
 using ShoppingSystem.Basket.API.Models;
 using ShoppingSystem.Basket.API.Repositories;
 using ShoppingSystem.BuildingBlocks.Behaviors;
@@ -23,13 +27,29 @@ builder.Services.AddMarten(options =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("PostGreSqlDatabase"))
+    .AddRedis(builder.Configuration.GetConnectionString("RedisConnection"));
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
